5097a952e31787f2340f8d06d4feb13d6e977566
Jack Andrew Wiegman <wiegmanja@s.dcsdk12.org>
2018-08-17 07:56:23 -0600

    :

---
+++ Chapter9/ValueOfAQuadradic/ValueOfAQuadradic.java
@@ -0,0 +1,9 @@
+public class ValueOfAQuadradic {
+
+	public static void main(String[] args) {
+		double x = 4;
+		double value = (3*(Math.pow(x, 2))) - (8*x) + 4;
+		System.out.println("At x = " + x + " the value is " + value);
+	}
+
+}
