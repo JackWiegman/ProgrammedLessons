aa84ace0d4a8cb1a3525d5b8d843c76d13d6e646
Jack Andrew Wiegman <wiegmanja@s.dcsdk12.org>
2018-08-21 08:27:03 -0600

    find sin and cos of number, square both and add the squares together

---
+++ Chapter10/Trigonometry/Trigonometry.java
@@ -0,0 +1,10 @@
+public class Trigonometry {
+
+	public static void main(String[] args) {
+		double sin = Math.sin(.5236);
+		double cos = Math.cos(.5236);
+		double squareSum = (Math.pow(sin, 2)) + (Math.pow(cos,2));
+		System.out.println("sine: " + sin + "\ncosine: " + cos + "\nsum of squares: " + squareSum);
+	}
+
+}
