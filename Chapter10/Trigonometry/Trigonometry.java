62da5910e66550afdfa93f6d04eac5fa9cdc652d
Jack Andrew Wiegman <wiegmanja@s.dcsdk12.org>
2018-08-22 08:12:20 -0600

    convert angle from degrees to radians

--- Chapter10/Trigonometry/Trigonometry.java
+++ Chapter10/Trigonometry/Trigonometry.java
@@ -1,7 +1,8 @@
 public class Trigonometry {
 
 	public static void main(String[] args) {
-		double num = 0.866;
+		double num = 30;
+		num = num * (Math.PI / 180);
 		double sin = Math.sin(num);
 		double cos = Math.cos(num);
 		double squareSum = (Math.pow(sin, 2)) + (Math.pow(cos,2));
