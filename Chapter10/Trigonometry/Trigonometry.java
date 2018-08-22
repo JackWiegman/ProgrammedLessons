ebe11e9f3ed4724c02693e4f36020af14d6b2bf6
Jack Andrew Wiegman <wiegmanja@s.dcsdk12.org>
2018-08-22 08:04:40 -0600

    Make variable for number that is sined and cosed, try out different numbers

--- Chapter10/Trigonometry/Trigonometry.java
+++ Chapter10/Trigonometry/Trigonometry.java
@@ -1,8 +1,9 @@
 public class Trigonometry {
 
 	public static void main(String[] args) {
-		double sin = Math.sin(.5236);
-		double cos = Math.cos(.5236);
+		double num = 0.866;
+		double sin = Math.sin(num);
+		double cos = Math.cos(num);
 		double squareSum = (Math.pow(sin, 2)) + (Math.pow(cos,2));
 		System.out.println("sine: " + sin + "\ncosine: " + cos + "\nsum of squares: " + squareSum);
 	}
