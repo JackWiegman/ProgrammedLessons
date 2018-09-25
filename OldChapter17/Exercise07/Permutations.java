fdd554b37cb47a7acc1247d350913d9de23bc965
Jack Wiegman <jawiegman@gmail.com>
2018-09-25 08:01:12 -0600

    Find how many permutations of n objects taken r at a time, with n and r being inputted by user

--- OldChapter17/Exercise07/Permutations.java
+++ OldChapter17/Exercise07/Permutations.java
@@ -3,7 +3,37 @@ import java.util.Scanner;
 public class Permutations {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);	
+		System.out.println("Enter N: ");
+		int n = scan.nextInt();
+		System.out.println("Enter R: ");
+		int r = scan.nextInt();
+
+		long nFactorial = n;
+		if (n >= 0) {
+			for (int i = n - 1; i > 1; i--) {
+				nFactorial *= i;
+			}
+		} else {
+			System.out.println("N can't be negative.");
+		}
+
+		int difference = n - r;
+		long differenceFactorial = difference;
+		if (n >= 0 && r >= 0) {
+			if (n > r) {
+				for (int i = difference - 1; i > 1; i--) {
+					differenceFactorial *= i;
+				}
+				long total = nFactorial / differenceFactorial;
+				System.out.println("Number of permutations is " + total);
+			} else {
+				System.out.println("R must be less than or equal to N.");
+			}
+		} else {
+			System.out.println("R can't be negative.");
+		}
+
 	}
 
 }
