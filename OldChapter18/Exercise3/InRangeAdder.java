e83803c6a15267a2d4bbf70b7494b7211ca3e9dc
Jack Wiegman <jawiegman@gmail.com>
2018-09-25 21:27:49 -0600

    add numbers together if within range and return sum of in range and out of range

--- OldChapter18/Exercise3/InRangeAdder.java
+++ OldChapter18/Exercise3/InRangeAdder.java
@@ -10,14 +10,20 @@ public class InRangeAdder {
 		int highEnd = scan.nextInt();
 
 		int num = 1;
-		int numSum = 0;
+		int inRangeSum = 0;
+		int outRangeSum = 0;
 		while (num != 0) {
 			System.out.println("Enter data: ");
 			num = scan.nextInt();
-			if (num >= lowEnd && num <= highEnd) {
-				
-			}
+			if (num != 0) {
+				if (num >= lowEnd && num <= highEnd) {
+					inRangeSum += num;
+				}
+				outRangeSum += num;
+			} 
 		}
+		System.out.println("Sum of in range values: " + inRangeSum);
+		System.out.println("Sum of out of range values: " + outRangeSum);
 	}
 
 }
