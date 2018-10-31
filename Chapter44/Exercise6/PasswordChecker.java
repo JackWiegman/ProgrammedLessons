3b22375b957717807bd34a375cbd8043bbd1dcac
Jack Wiegman <jawiegman@gmail.com>
2018-10-31 11:48:40 -0600

    wrong things please let this merge

--- Chapter44/Exercise6/PasswordChecker.java
+++ Chapter44/Exercise6/PasswordChecker.java
@@ -4,16 +4,42 @@ public class PasswordChecker {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter your password: ");
-		String password = scan.nextLine();
+		
 
 		String errorMessage = "That password is not acceptable.";
+		boolean hasUpperCase = false;
+		boolean hasLowerCase = false;
+		boolean hasNumber = false;
+		boolean acceptablePassword = false;
 
-		if (password.length() >= 7) {
-			
-		} else {
-			System.out.println(errorMessage);
+		while (!acceptablePassword) {
+			System.out.println("Enter your password: ");
+			String password = scan.nextLine();
+
+			if (password.length() >= 7) {
+				for (int i = 0; i < password.length(); i++) {
+					if (Character.isUpperCase(password.charAt(i))) {
+						hasUpperCase = true;
+					} 
+					if (Character.isLowerCase(password.charAt(i))) {
+						hasLowerCase = true;
+					}
+					if (Character.isDigit(password.charAt(i))) {
+						hasNumber = true;
+					}
+				}
+				if (hasUpperCase && hasLowerCase && hasNumber) {
+					System.out.println("Acceptable password.");
+					acceptablePassword = true;
+				} else {
+					System.out.println(errorMessage);
+				}
+			} else {
+				System.out.println(errorMessage);
+			}
 		}
+
+
 	}
 
 }
