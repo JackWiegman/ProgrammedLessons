9dbdec695ef058290b16bf7f3bad4ea039f6d525
Jack Wiegman <jawiegman@gmail.com>
2018-11-04 22:00:24 -0700

    fixed the box

--- Chapter50/Exercise1/TestBox.java
+++ Chapter50/Exercise1/TestBox.java
@@ -2,17 +2,14 @@ class TestBox {
 
 	public static void main (String[] args) {
 		Box box = new Box(2.5, 5.0, 6.0);
-<<<<<<< HEAD
 
 		System.out.println("Area: " + box.area() + " volume: " + box.volume());
 		System.out.printlnl("Area: " + smallerBox.area());
-=======
+
 		biggerBox = box.biggerBox();
 
 		System.out.println("Area: " + box.area() + " volume: " + box.volume());
 		System.out.println("Area: " + biggerBox.area());
->>>>>>> 71bb8e44a1516440f7c707612078ef317203dd6b
-
 	}
 
 }
