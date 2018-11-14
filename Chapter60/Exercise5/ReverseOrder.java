8c2936f895abf1cbdbdba6e6d531220d396f9ee9
Jack Wiegman <jawiegman@gmail.com>
2018-11-14 08:55:06 -0700

    Only use one array, rather than adding numbers to another

--- Chapter60/Exercise5/ReverseOrder.java
+++ Chapter60/Exercise5/ReverseOrder.java
@@ -3,17 +3,18 @@ public class ReverseOrder {
 	public static void main (String[] args) {
 		int[] val = {0,1,2,3};
 		int temp;
-		int[] reverse = {0,0,0,0};
 
 		System.out.println( "Original Array: " 
         	+ val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
 
-		for (int i = 0; i < 4; i++) {
-			reverse[3 - i] = val[i];
+		for (int i = 0; i < 2; i++) {
+			temp = val[i];
+			val[i] = val[3 - i];
+			val[3 - i] = temp;
 		}
 
 		System.out.println( "Reversed Array: " 
-        	+ reverse[0] + " " + reverse[1] + " " + reverse[2] + " " + reverse[3] );
+        	+ val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
 
 	}
 
