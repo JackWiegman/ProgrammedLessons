6857aa5a9d155c30de6ef612343a6075f82b8251
Jack Wiegman <jawiegman@gmail.com>
2018-11-16 07:58:25 -0700

    Start Reversal1

---
+++ Chapter61/Exercise4/Reversal1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Reversal1 {
+
+	public static void main (String[] args) {
+		int[] data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+
+		
+		
+		for (int i = 0; i < data.length / 2; i++) {
+			
+		}
+
+	}
+
+}
