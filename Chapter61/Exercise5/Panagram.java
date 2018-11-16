58d6a879719dd879aa9e3cc3399c5a03420dd5e9
Jack Wiegman <jawiegman@gmail.com>
2018-11-16 08:26:47 -0700

    Start panagram tester

---
+++ Chapter61/Exercise5/Panagram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Panagram {
+
+	public static void main (String[] args) {
+		int[] letterCount = new int[25];
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter a sentence: ");
+		String sentence = scan.nextLine();
+
+		for (int i = 0; i <= letterCount.length; i++) {
+			int letter = 
+		}
+
+
+
+	}
+
+}
